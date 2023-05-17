aa8a4d4b8c7a23d063a0dbaa2ef35bc9ab0f85d2
johnnyofoco <johnny.marlon.paula@gmail.com>
2023-05-17 20:36:45 -0300

    Add javaScript on site for menu mobile and general navigation

--- Projeto/js/scripts.js
+++ Projeto/js/scripts.js
@@ -1,1 +1,39 @@
-console.log('Teste!')
+// Elements selection
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+// Functions
+function smoothScroll(e) {
+  e.preventDefault();
+
+  const href = this.getAttribute("href");
+  const offSetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offSetTop,
+    behavior: "smooth",
+  });
+  
+  setTimeout(()=>{
+    if(menu.classList.contains("menu-active")){
+        menu.classList.remove("menu-active")
+    }
+  }, 500)
+
+}
+
+// Events
+[menuBtn, closeMenuBtn].forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    menu.classList.toggle("menu-active");
+  });
+});
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", smoothScroll);
+});
