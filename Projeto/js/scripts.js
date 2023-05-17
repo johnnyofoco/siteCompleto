39964f8c3df94fa8b626ed2d1ce774bad8d7eb8c
johnnyofoco <johnny.marlon.paula@gmail.com>
2023-05-17 20:57:17 -0300

    Applying js for the slider

--- Projeto/js/scripts.js
+++ Projeto/js/scripts.js
@@ -7,6 +7,10 @@ const desktopLinks = document.querySelectorAll("#navbar a");
 const mobileLinks = document.querySelectorAll("#mobile-navbar a");
 const allLinks = [...desktopLinks, ...mobileLinks];
 
+const slides = document.querySelectorAll(".banner");
+const dots = document.querySelectorAll(".dot");
+let slideIndex = 0;
+
 // Functions
 function smoothScroll(e) {
   e.preventDefault();
@@ -18,13 +22,30 @@ function smoothScroll(e) {
     top: offSetTop,
     behavior: "smooth",
   });
-  
-  setTimeout(()=>{
-    if(menu.classList.contains("menu-active")){
-        menu.classList.remove("menu-active")
-    }
-  }, 500)
 
+  setTimeout(() => {
+    if (menu.classList.contains("menu-active")) {
+      menu.classList.remove("menu-active");
+    }
+  }, 500);
+}
+
+function showSlides() {
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    dots[i].classList.remove("active");
+  }
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].classList.add("active");
+  dots[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 5000)
 }
 
 // Events
@@ -37,3 +58,6 @@ function smoothScroll(e) {
 allLinks.forEach((link) => {
   link.addEventListener("click", smoothScroll);
 });
+
+// Inicialização
+showSlides();
